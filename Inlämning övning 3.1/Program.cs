c5dadb8e991f7aac5ebff391026f33ccdd8ab939
ahka07001 <ahka07001@E-PW05CGQT>
2024-11-07 15:50:56 +0100

    Add project files.

---
+++ Inlämning övning 3.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Inlämning_._Övning_._3_._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder < 16)
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            else if (ålder > 19)
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            else 
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+                    
+        }
+    }
+}
